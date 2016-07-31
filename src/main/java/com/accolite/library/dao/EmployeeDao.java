e69f84e2459c137338813f5bf586dd1b1c73fc37
git <saibalaji.polakampally@accoliteindia.com>
2016-07-31 14:51:04 +0530

    implemented updating an employee details and getting the details of an employee using emailId

--- src/main/java/com/accolite/library/dao/EmployeeDao.java
+++ src/main/java/com/accolite/library/dao/EmployeeDao.java
@@ -16,12 +16,14 @@
 package com.accolite.library.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
 import com.accolite.library.model.Employee;
@@ -57,8 +59,9 @@ public class EmployeeDao {
 	}
 
 	public boolean createUser(final Employee employee) {
-		
-		//SQL query to inset values into the LibraryUser table which contains all the employee details
+
+		// SQL query to inset values into the LibraryUser table which contains
+		// all the employee details
 		String sql = "insert into LibraryUser(emailId, employeeId, googleId, firstName, middleName, lastName, ManagerId, MobileNo, cityId, password) values(?,?,?,?,?,?,?,?,?,?)";
 
 		return jdbcTemplate.execute(sql, new PreparedStatementCallback<Boolean>() {
@@ -75,11 +78,48 @@ public class EmployeeDao {
 				ps.setString(8, employee.getMobileNo());
 				ps.setString(9, employee.getCityId());
 				ps.setString(10, employee.getPassword());
-				
+
 				return ps.execute();
 			}
 
-		}); //End of return statement
+		}); // End of return statement
+	}
+
+	public int updateUser(Employee employee) {
+		String sql = "update LibraryUser set employeeid = ?, googleId = ?, firstName = ?, middleName = ?, lastName = ?, ManagerId = ?, MobileNo = ?, cityId = ?, password = ?  where emailId = ?";
+
+		return jdbcTemplate.update(sql, employee.getEmployeeId(), employee.getGoogleId(), employee.getFirstName(),
+				employee.getMiddleName(), employee.getLastName(), employee.getManagerId(), employee.getMobileNo(),
+				employee.getCityId(), employee.getPassword(), employee.getEmailId());
+	}
+	
+	public Employee getEmployee(String emailId){
+		String sql = "select * from LibraryUser where emailId = " + emailId;
+		
+		return jdbcTemplate.query(sql, new ResultSetExtractor<Employee>(){
+
+			public Employee extractData(ResultSet rs) throws SQLException, DataAccessException {
+				Employee employee = new Employee();
+				while(rs.next()){
+					employee.setEmailId(rs.getString("emailId"));
+					employee.setEmployeeId(rs.getString("employeeId"));
+					employee.setGoogleId(rs.getString("googleId"));
+					employee.setFirstName(rs.getString("firstName"));
+					employee.setMiddleName(rs.getString("middleName"));
+					employee.setLastName(rs.getString("lastName"));
+					employee.setManagerId(rs.getString("ManagerId"));
+					employee.setMobileNo(rs.getString("MobileNo"));
+					employee.setCityId(rs.getString("cityId"));
+					employee.setPassword(rs.getString("password"));
+				}
+				return employee;
+			}
+			
+		});
+	}
+	
+	public boolean blackListEmployee(){
+		
 	}
 
 }
